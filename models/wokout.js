158fb50ba74b6f36be47c10301805f8c18829f44
creyes25 <carolina.reyes2022@gmail.com>
2022-10-02 13:54:45 -0500

    set up all the workout model

---
+++ models/wokout.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const setSchema = new Schema({
+  setNum: Number,
+  reps: Number,
+  weight: Number
+})
+
+
+
+const exerciseSchema = new Schema({
+  name: String,
+  exerciseType: String,
+  sets: [setSchema]
+})
+
+
+const workoutSchema = new Schema({
+  date: Date,
+  difficulty: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  note: String,
+  exercises: [exerciseSchema],
+  profile: { type: Schema.Types.ObjectId, ref: 'Profile' },
+}, {
+  timestamps: true,
+})
+
+const Workout = mongoose.model('Workout', workoutSchema)
+
+export {
+  Workout
+}
