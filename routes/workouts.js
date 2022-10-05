b3bd540a7785012af4a65fa0ef4bb079a080e8ba
creyes25 <carolina.reyes2022@gmail.com>
2022-10-05 10:10:38 -0500

    add authorization, where you have to be logged in to access features of the app

--- routes/workouts.js
+++ routes/workouts.js
@@ -6,37 +6,37 @@ import * as workoutsCtrl from '../controllers/workouts.js'
 const router = Router()
 
 // GET /workouts/new -- create a new workout -- (isloggedin)
-router.get('/new', workoutsCtrl.new)
+router.get('/new', isLoggedIn, workoutsCtrl.new)
 
 // GET - /workouts -- view all workouts 
-router.get('/', workoutsCtrl.index)
+router.get('/', isLoggedIn, workoutsCtrl.index)
 
 //GET - /workouts/:id -- view details of individual workout ()
-router.get('/:id', workoutsCtrl.show)
+router.get('/:id', isLoggedIn, workoutsCtrl.show)
 
 // GET - /workouts/:id/edit
-router.get('/:id/edit', workoutsCtrl.edit)
+router.get('/:id/edit', isLoggedIn, workoutsCtrl.edit)
 
 // GET - /workouts/:workoutId/exercises/:exerciseId/newset
-router.get('/:workoutId/exercises/:exerciseId/newset', workoutsCtrl.newSet)
+router.get('/:workoutId/exercises/:exerciseId/newset', isLoggedIn, workoutsCtrl.newSet)
 
 // GET - /workouts/:workoutId/exercises/:exerciseId -- get details on certain exercise
-router.get('/:workoutId/exercises/:exerciseId',  workoutsCtrl.showExercise)
+router.get('/:workoutId/exercises/:exerciseId',  isLoggedIn, workoutsCtrl.showExercise)
 
 // POST - /workouts -- post the new workout into database ()
-router.post('/', workoutsCtrl.create)
+router.post('/', isLoggedIn, workoutsCtrl.create)
 
 // POST - /workouts/:id/exercises -- post new exercise into the workout
-router.post('/:id/exercises', workoutsCtrl.createExercise)
+router.post('/:id/exercises', isLoggedIn, workoutsCtrl.createExercise)
 
 // POST - /workouts/:workoutId/exercises/:exerciseId/create -- post a new set into an exercise
-router.post('/:workoutId/exercises/:exerciseId/create', workoutsCtrl.createSet)
+router.post('/:workoutId/exercises/:exerciseId/create', isLoggedIn,  workoutsCtrl.createSet)
 
 // PUT - /workouts/:id  -- update workout
-router.put('/:id', workoutsCtrl.update)
+router.put('/:id', isLoggedIn, workoutsCtrl.update)
 
 // DELETE - /workouts/:id -- delete a workout
-router.delete('/:id', workoutsCtrl.delete)
+router.delete('/:id', isLoggedIn, workoutsCtrl.delete)
 
 export {
   router
